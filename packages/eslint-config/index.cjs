f6c60f0007bc8d600570cd4a27b4408eed70713f
webxrahat <islammdrahat308@gmail.com>
2025-12-21 08:51:06 +0600

    monorepo

---
+++ packages/eslint-config/index.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  env: {
+    node: true,
+    es2022: true
+  },
+  rules: {
+    'no-console': 'off'
+  }
+}
--- src/app.ts
+++
@@ -1,6 +0,0 @@
-import express from "express";
-
-const app = express();
-app.use(express.json({ limit: "10mb" })); // Adjust limit based on your requirements
-
-export default app;
